33c223679c6c9e642f6182ba294ad7ef20b96e18
lucasgomesnog <lucasgomes145987@gmail.com>
2025-05-20 16:15:54 -0300

    changedlayout

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -59,7 +59,7 @@ const AnnouncementListPage = () => {
       {/* TOP */}
       <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-lg font-semibold">
-          All Announcements
+          Lembretes
         </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -6,7 +6,7 @@ const TableSearch = () => {
       <Image src="/search.png" alt="" width={14} height={14} />
       <input
         type="text"
-        placeholder="Search..."
+        placeholder="Procurar..."
         className="w-[200px] p-2 bg-transparent outline-none"
       />
     </div>
